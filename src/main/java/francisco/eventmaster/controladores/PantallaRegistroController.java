f0867cf91af268ed19276823917f92a97e9b0ee8
2damb <2damb@PC234013.informatica.edu>
2023-11-27 18:12:58 +0000

    pantalla registro terminada

--- src/main/java/francisco/eventmaster/controladores/PantallaRegistroController.java
+++ src/main/java/francisco/eventmaster/controladores/PantallaRegistroController.java
@@ -1,26 +1,78 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package francisco.eventmaster.controladores;
 
+import francisco.eventmaster.App;
+import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DateCell;
+import javafx.scene.control.DatePicker;
+import javafx.scene.layout.Background;
+import javafx.util.Callback;
+import javafx.util.StringConverter;
 
-/**
- * FXML Controller class
- *
- * @author 2damb
- */
+
 public class PantallaRegistroController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Button goBackButton;
+    @FXML
+    private DatePicker bornDate;
+    @FXML
+    private Button registerButton;
+
+   
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+         bornDate.setConverter(new StringConverter<LocalDate>(){
+             DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+             @Override
+             public String toString(LocalDate date) {
+                 if (date != null) {
+                    return dateFormatter.format(date);
+                }
+                    return "";
+             }
+
+             @Override
+             public LocalDate fromString(String string) {
+                 if (string != null && !string.isEmpty()) {
+                    return LocalDate.parse(string, dateFormatter);
+                }
+                    return null;
+             }  
+         }); 
+              bornDate.setDayCellFactory(new Callback<DatePicker, DateCell>() {
+            @Override
+            public DateCell call(DatePicker param) {
+                return new DateCell() {
+                    @Override
+                    public void updateItem(LocalDate item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (item != null) {
+                                setStyle("-fx-text-fill: #004AAD;");
+                        }
+                    }
+                };
+            }
+        });
     }    
+
+    @FXML
+    private void onGoBackButton(ActionEvent event) throws IOException {
+        App.setRoot("PantallaInicio");
+    }
+    
+
+    @FXML
+    private void onRegisterButton(ActionEvent event) {
+        //TODO
+    }
     
 }
